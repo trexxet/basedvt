fa575cbdd21c5c4b93a4069ce64ad5dbef23786d
trexxet <atomnik07@gmail.com>
2026-04-19 23:48:42 +0500

    Demo: refactor
    - decode in both INPUT & STRICT modes

--- demo/demoVT.cpp
+++ demo/demoVT.cpp
@@ -7,13 +7,16 @@
 #include <span>
 #include <string>
 
-#include <windows.h>
-
-#include <io.h>
-
 #include "Basedlib/Core/FSM.hpp"
+#include "Basedlib/Meta/PrettyEnum.hpp"
 
 // TODO: linux
+
+#ifdef __WIN32
+#include <windows.h>
+#include <io.h>
+// bruh
+#undef STRICT
 
 struct ConfTerm {
 	HANDLE hIn;
@@ -35,6 +38,7 @@ bool conf_term (ConfTerm& ct) {
 void unconf_term (ConfTerm& ct) {
 	SetConsoleMode (ct.hIn, ct.modeSave);
 }
+#endif
 
 ssize_t read_input (std::span<uint8_t> buf) {
 	ssize_t n = read (STDIN_FILENO, buf.data(), buf.size());
@@ -48,34 +52,50 @@ ssize_t read_input (std::span<uint8_t> buf) {
 	return n;
 }
 
+void print_decoded_key (const BasedVT::KeyInput& key) {
+	std::string str = std::format ("Done: {}", BasedVT::KeyInput::PrettyKey::to_string (key.key));
+	if (key.key == BasedVT::KeyInput::Key::CHAR)
+		str.append (std::isspace (key.ch) ? " whitespace" : std::format(" {}", key.ch));
+	if (key.ctrl) str.append (" ctrl");
+	if (key.alt) str.append (" alt");
+	if (key.shift) str.append (" shift");
+	std::print ("{}\n", str);
+}
+
+void decode (BasedVT::Parser& parser, std::span<uint8_t> buf, BasedVT::Tokenizer::Mode mode) {
+	bool decodedAny = false;
+	parser.switch_mode (mode);
+	std::print ("Mode {}: ", Basedlib::PrettyEnum<BasedVT::Tokenizer::Mode>::to_string (mode));
+
+	for (uint8_t b : buf) {
+		parser.feed (b);
+		auto key = (buf.size() == 1 && b == '\e') ? parser.flush() : parser.get();
+		if (key) {
+			print_decoded_key (*key);
+			decodedAny = true;
+		}
+	}
+
+	if (!decodedAny)
+		std::print ("No key decoded\n");
+}
+
 int main () {
+#ifdef __WIN32
 	ConfTerm ct;
-	if (!conf_term (ct)) return 0;
+	if (!conf_term (ct)) return 1;
+#endif
 	std::print ("BasedVT Demo\n");
 
 	uint8_t buf[64];
 	ssize_t n = read_input (buf);
 
-	BasedVT::Parser vtParser (BasedVT::Tokenizer::Mode::INPUT);
+	BasedVT::Parser parser (BasedVT::Tokenizer::Mode::INPUT);
+	decode (parser, std::span (buf, n), BasedVT::Tokenizer::Mode::INPUT);
+	decode (parser, std::span (buf, n), BasedVT::Tokenizer::Mode::STRICT);
 
-	bool decodedAny = false;
-	for (ssize_t i = 0; i < n; i++) {
-		vtParser.feed (buf[i]);
-		auto key = (n == 1 && buf[i] == '\e') ? vtParser.flush() : vtParser.get();
-		if (key) {
-			std::string str = std::format ("Done: {}", BasedVT::KeyInput::PrettyKey::to_string (key->key));
-			if (key->key == BasedVT::KeyInput::Key::CHAR)
-				str.append (std::isspace (key->ch) ? " whitespace" : std::format(" {}", key->ch));
-			if (key->ctrl) str.append (" ctrl");
-			if (key->alt) str.append (" alt");
-			if (key->shift) str.append (" shift");
-			std::print ("{}\n", str);
-			decodedAny = true;
-		}
-	}
-	if (!decodedAny)
-		std::print ("No key decoded\n");
-
+#ifdef __WIN32
 	unconf_term (ct);
+#endif
 	return 0;
 }
