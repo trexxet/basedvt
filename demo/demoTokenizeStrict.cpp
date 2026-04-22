9580fcae11f849f831c03cfbada4634124c23fa3
trexxet <atomnik07@gmail.com>
2026-04-23 00:41:51 +0500

    Demo: add reading argv in demoTokenizeStrict

--- demo/demoTokenizeStrict.cpp
+++ demo/demoTokenizeStrict.cpp
@@ -1,6 +1,8 @@
 #include "demoCommon.hpp"
 #include "BasedVT/Tokenizer.hpp"
 
+#include <cstring>
+
 static_assert (BASEDVT_ENABLE_TO_STRING);
 
 bool needs_flush (std::span<uint8_t> buf, uint8_t currByte) {
@@ -22,7 +24,7 @@ void tokenize (BasedVT::Tokenizer& tokenizer, std::span<uint8_t> buf) {
 		std::print ("No token obtained\n");
 }
 
-int main () {
+int main (int argc, char* argv[]) {
 #ifdef __WIN32
 	ConfTerm ct;
 	if (!conf_term (ct)) return 1;
@@ -30,7 +32,18 @@ int main () {
 	std::print ("BasedVT Tokenizer Strict mode demo\n");
 
 	uint8_t buf[64];
-	ssize_t n = read_input (buf);
+	ssize_t n = 0;
+
+	if (argc == 1) {
+		std::print ("No input arg - reading key input\n");
+		n = read_input (buf);
+	} else if (argc == 2 && argv[1]) {
+		std::print ("Reading argv[1]\n");
+		n = std::min (std::strlen (argv[1]), sizeof(buf));
+		std::memcpy (buf, argv[1], n);
+	} else {
+		std::print ("Bad input\n");
+	}
 
 	BasedVT::Tokenizer tokenizer (BasedVT::Tokenizer::Mode::STRICT);
 	tokenize (tokenizer, std::span (buf, n));
